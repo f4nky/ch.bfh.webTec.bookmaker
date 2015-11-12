bcb488f26be92c95eb4ed4c43a06ffc87a0509f3
joe1990 <holzerjoel@gmail.com>
2015-11-12 10:34:08 +0100

    - Commented classes - Added all crud-test-methods for the user-class - Update persistence.xml to avoid deprecated warnings

--- Bookmaker/src/main/java/model/User.java
+++ Bookmaker/src/main/java/model/User.java
@@ -6,73 +6,133 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 /**
- * Created by Fanky on 29.10.15.
+ * Model for user-objects.
+ * Is a jpa entity (jpa annotations) to get, store and update user in the database.
+ * <br/><br/>
+ *
+ * <b>History:</b>
+ * <pre>
+ * 1.0	29.10.2015	Michael Fankhauser  Class created.
+ * 1.1  12.11.2015  Joel Holzer         Format code, Added comments.
+ * </pre>
+ *
+ * @author Michael Fankhauser, Joel Holzer
+ * @version 1.1
+ * @since 12.11.2015
  */
 @Entity
 public class User {
     @GeneratedValue
     @Id
     private Long id;
+    @Basic
+    private String email;
+    @Basic
+    private String firstName;
+    @Basic
+    private String lastName;
+    @Basic
+    private String password;
+    @Basic
+    private Boolean isAdmin;
 
+    /**
+     * Returns the unique id of the user.
+     * @return Unique id of the user.
+     * @since 29.10.2015
+     */
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Basic
-    private String email;
-
+    /**
+     * Returns the email of the user.
+     * @return Email of the user.
+     * @since 29.10.2015
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Sets the email of the user.
+     * @param email Email to set.
+     * @since 29.10.2015
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
-    @Basic
-    private String firstName;
-
+    /**
+     * Returns the first name of the user.
+     * @return First name of the user.
+     * @since 29.10.2015
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Sets the first name of the user.
+     * @param firstName First name to set.
+     * @since 29.10.2015
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
-    @Basic
-    private String lastName;
-
+    /**
+     * Returns the last name of the user.
+     * @return Last name of the user.
+     * @since 29.10.2015
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Sets the last name of the user.
+     * @param lastName Last name to set.
+     * @since 29.10.2015
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
-    @Basic
-    private String password;
-
+    /**
+     * Returns the password of the user.
+     * @return Password of the user.
+     * @since 29.10.2015
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Sets the password of the user.
+     * @param password Password to set.
+     * @since 29.10.2015
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
-    @Basic
-    private Boolean isAdmin;
-
+    /**
+     * Returns the isAdmin-Flag of the user.
+     * If this flag is set to true, then the user is an admin, else the user is a normal user.
+     * @return isAdmin-Flag of the user. true = admin user, false = normal user.
+     * @since 29.10.2015
+     */
     public Boolean getIsAdmin() {
         return isAdmin;
     }
 
+    /**
+     * Sets the isAdmin-Flag of the user.
+     * If this flag is set to true, then the user is an admin, else the user is a normal user.
+     * @param isAdmin Value to set for the isAdmin-Flag. true = Admin user, false = normal user.
+     * @since 29.10.2015
+     */
     public void setIsAdmin(Boolean isAdmin) {
         this.isAdmin = isAdmin;
     }
--- Bookmaker/src/main/test/java/model/UserTest.java
+++ Bookmaker/src/main/test/java/model/UserTest.java
@@ -5,11 +5,34 @@ import org.junit.Test;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 import java.text.ParseException;
+import java.util.List;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit Tests for the user model class.
+ * Tests the crud db-operations (create, read, update, delete).
+ * <br/><br/>
+ *
+ * <b>History:</b>
+ * <pre>
+ * 1.0	29.10.2015	Michael Fankhauser  Class created.
+ * 1.1  12.11.2015  Joel Holzer         Added methods to test read, update and delete.
+ * </pre>
+ *
+ * @author Michael Fankhauser, Joel Holzer
+ * @version 1.1
+ * @since 12.11.2015
+ */
 public class UserTest {
 
+    /**
+     * Tests the creation of a user in the database.
+     * @since 29.10.2015
+     */
     @Test
-    public void test() throws ParseException {
+    public void testCreate() throws ParseException {
         User user = new User();
         user.setEmail("test@test.ch");
         user.setFirstName("John");
@@ -20,6 +43,59 @@ public class UserTest {
 
         em.getTransaction().begin();
         em.persist(user);
+        em.getTransaction().commit();
+    }
+
+    /**
+     * Tests the read (select) of a user from the database.
+     * @since 12.11.2015
+     */
+    @Test
+    public void testRead() throws ParseException {
+        testCreate();
+        EntityManager em = Persistence.createEntityManagerFactory("BMUnit").createEntityManager();
+
+        List<User> users = em.createQuery("select u from User u").getResultList();
+        User user = users.get(0);
+        assertNotNull(user);
+    }
+
+    /**
+     * Tests the update of a user in the database.
+     * @since 12.11.2015
+     */
+    @Test
+    public void testUpdate() throws ParseException {
+        testCreate();
+        EntityManager em = Persistence.createEntityManagerFactory("BMUnit").createEntityManager();
+
+        List<User> users = em.createQuery("select u from User u").getResultList();
+        User user = users.get(0);
+
+        em.getTransaction().begin();
+        {
+            user.setFirstName("NewFirstName");
+        }
+        em.getTransaction().commit();
+
+        assertTrue(user.getFirstName().equals("NewFirstName"));
+    }
+
+    /**
+     * Tests the deletion of a user in the database.
+     * @since 12.11.2015
+     */
+    @Test
+    public void testDelete() {
+        EntityManager em = Persistence.createEntityManagerFactory("BMUnit").createEntityManager();
+
+        List<User> users = em.createQuery("select u from User u").getResultList();
+        User user = users.get(0);
+
+        em.getTransaction().begin();
+        {
+            em.remove(user);
+        }
         em.getTransaction().commit();
     }
 }
